b6adf911c3f842ee5e72242081982f8000113745
Alejandro Sánchez Roncero <alexsanchezronceros@gmail.com>
2024-03-12 10:35:05 +0100

    Trying to improve metrics of 5m_vs_6m and 6h_vs_8z

--- research/qmix_tl.py
+++ research/qmix_tl.py
@@ -12,8 +12,10 @@ from main_plot import generate_training_figure
 # generate_training_figure(["25m_rnn_qmix", "25m_from-3m_rnn_qmix_unfreeze0.2", "25m_from-8m_rnn_qmix_unfreeze0.2",
 #                           "25m_from-25m_rnn_qmix_unfreeze0.2", "25m_from-5m_rnn_qmix_direct"], label="tl_qmix/25m_rnn_qmix.png", only_last_legend=True)
 
-# generate_training_figure(["5m_vs_6m_rnn_qmix", "5m_vs_6m_from-3m_rnn_qmix_unfreeze0.2", "5m_vs_6m_from-8m_rnn_qmix_unfreeze0.2",
-#                           "5m_vs_6m_from-25m_rnn_qmix_unfreeze0.2", "5m_vs_6m_from-5m_rnn_qmix_unfreeze0.2"], label="tl_qmix/5m_vs_6m_rnn_qmix.png", only_last_legend=True)
+# generate_training_figure(["5m_vs_6m_rnn_qmix", "5m_vs_6m_rnn_qmix_ext", "5m_vs_6m_from-3m_rnn_qmix_direct", "5m_vs_6m_from-3m_rnn_qmix_unfreeze0.2", 
+#                           "5m_vs_6m_from-8m_rnn_qmix_unfreeze0.2",
+#                           "5m_vs_6m_from-25m_rnn_qmix_unfreeze0.2", "5m_vs_6m_from-5m_rnn_qmix_unfreeze0.2",
+#                           ], label="tl_qmix/5m_vs_6m_rnn_qmix.png", only_last_legend=True)
 
 
 ############ BLOCK 2 ############
@@ -28,8 +30,8 @@ from main_plot import generate_training_figure
 # generate_training_figure(["3s_vs_3z_rnn_qmix", "3s_vs_3z_from-3m_rnn_qmix_unfreeze0.2", "3s_vs_3z_from-3s_rnn_qmix_unfreeze0.2",
 #                           ], label="tl_qmix/3s_vs_3z_rnn_qmix.png", only_last_legend=True)
 
-# generate_training_figure(["3s_vs_4z_rnn_qmix", "3s_vs_4z_from-3s_rnn_qmix_unfreeze0.2",
-#                           ], label="tl_qmix/3s_vs_4z_rnn_qmix.png", only_last_legend=True)
+generate_training_figure(["3s_vs_4z_rnn_qmix", "3s_vs_4z_from-3s_rnn_qmix_unfreeze0.2",
+                          ], label="tl_qmix/3s_vs_4z_rnn_qmix.png", only_last_legend=True)
 
 
 ############ BLOCK 4 ############
@@ -37,5 +39,5 @@ from main_plot import generate_training_figure
 #                           ], label="tl_qmix/2m_vs_1z_rnn_qmix.png", only_last_legend=True)
 
 ############ BLOCK 5 ############
-generate_training_figure(["6h_vs_8z_rnn_qmix",
-                          ], label="tl_qmix/6h_vs_8z_rnn_qmix.png", only_last_legend=True)
+# generate_training_figure(["6h_vs_8z_rnn_qmix", "6h_vs_8z_rnn_qmix_ext", "6h_vs_8z_from-2m_rnn_qmix_direct", 
+#                           "6h_vs_8z_from-3s_rnn_qmix_direct"], label="tl_qmix/6h_vs_8z_rnn_qmix.png", only_last_legend=True)
